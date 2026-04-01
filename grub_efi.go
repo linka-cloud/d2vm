e53246b6e05a5ab5a7d04dd29fa6fc5ca1731539
Elias Abacioglu <1148206+Raboo@users.noreply.github.com>
2026-04-01 11:54:44 +0200

    refactor: remove dead copyEfiBinary code from grubEFI
    The --removable flag passed to grub2-install already places the EFI
    binary at the standard removable boot path. The copyEfiBinary function
    was redundant and always logged a warning about not finding the source
    file. Remove it along with unused imports.

--- grub_efi.go
+++ grub_efi.go
@@ -17,12 +17,8 @@ package d2vm
 import (
 	"context"
 	"fmt"
-	"os"
-	"path/filepath"
 
 	"github.com/sirupsen/logrus"
-
-	"go.linka.cloud/d2vm/pkg/exec"
 )
 
 type grubEFI struct {
@@ -49,41 +45,9 @@ func (g grubEFI) Setup(ctx context.Context, dev, root string, cmdline string) er
 	if err := g.install(ctx, "--target="+g.arch+"-efi", "--efi-directory=/boot", "--no-nvram", "--removable", "--no-floppy", "--force"); err != nil {
 		return err
 	}
-	if isRhelFamily(g.r.ID) {
-		if err := g.copyEfiBinary(ctx, root); err != nil {
-			return err
-		}
-	}
 	if err := g.mkconfig(ctx); err != nil {
 		return err
 	}
-	return nil
-}
-
-func (g grubEFI) copyEfiBinary(ctx context.Context, root string) error {
-	efiDir := filepath.Join(root, "boot", "efi", "EFI")
-	distroDirs := []string{"rocky", "almalinux", "centos", "rhel"}
-	var srcPath string
-	for _, d := range distroDirs {
-		p := filepath.Join(efiDir, d, "grub"+g.arch+".efi")
-		if _, err := os.Stat(p); err == nil {
-			srcPath = p
-			break
-		}
-	}
-	if srcPath == "" {
-		logrus.Warnf("could not find distro-specific EFI binary, skipping removable boot copy")
-		return nil
-	}
-	dstDir := filepath.Join(efiDir, "BOOT")
-	dstPath := filepath.Join(dstDir, "BOOT"+g.arch+".EFI")
-	if err := os.MkdirAll(dstDir, os.ModePerm); err != nil {
-		return fmt.Errorf("failed to create EFI boot directory: %w", err)
-	}
-	if err := exec.Run(ctx, "cp", srcPath, dstPath); err != nil {
-		return fmt.Errorf("failed to copy EFI binary to removable boot path: %w", err)
-	}
-	logrus.Infof("copied EFI binary to %s", dstPath)
 	return nil
 }
 
